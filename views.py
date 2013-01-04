326dafaa4262c155968659487478648af1081b31
Benjamin Cutler <cutler@cs.colostate.edu>
2013-01-03 20:56:15 -0700

    Added summary parameter to index page

--- views.py
+++ views.py
@@ -149,6 +149,8 @@ def index(request,event=None):
 		'choices' : Choice.objects.filter(**qf2).count(),
 		'donors' : Donor.objects.filter(**qf3).distinct().count(),
 	}
+	if 'json' in request.GET:
+		return HttpResponse(simplejson.dumps({'count':count,'agg':agg},ensure_ascii=False),content_type='application/json;charset=utf-8')
 	return tracker_response(request, 'tracker/index.html', { 'agg' : agg, 'count' : count, 'event': event })
 
 @never_cache
