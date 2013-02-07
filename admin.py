2eb87b37170c9e280c6e8103fe2c38ae0d319488
smk <smk@sda.sorrowind.net>
2013-02-06 22:34:58 -0600

    Made some modifications to the admin interface to show related entities, and to use a smarter foreign key assignment (uses a 'search' popup rather than a dropdown list, ideally we should be able to make it an auto-complete textbox, but this will work for now)

--- admin.py
+++ admin.py
@@ -1,30 +1,56 @@
 from django.contrib import admin
 import tracker.models
 
+class ChallengeInline(admin.StackedInline):
+	model = tracker.models.Challenge
+	extra = 0;
+
 class ChallengeAdmin(admin.ModelAdmin):
 	list_display = ('speedrun', 'name', 'goal', 'description', 'state')
 	list_editable = ('name', 'goal', 'state')
 	search_fields = ('name', 'speedrun__name', 'description')
+	raw_id_fields = ('speedrun',)
+	list_filter = ('speedrun__event', 'state')
 
 class ChallengeBidAdmin(admin.ModelAdmin):
 	list_display = ('challenge', 'donation', 'amount')
-
-class ChoiceAdmin(admin.ModelAdmin):
-	search_fields = ('name', 'speedrun__name', 'description');
+	raw_id_fields = ('challenge', 'donation')
 
 class ChoiceBidAdmin(admin.ModelAdmin):
 	list_display = ('option', 'donation', 'amount')
 
+class ChoiceOptionInline(admin.StackedInline):
+	model = tracker.models.ChoiceOption
+	extra = 0
+
 class ChoiceOptionAdmin(admin.ModelAdmin):
 	list_display = ('choice', 'name')
 	search_fields = ('name', 'description', 'choice__name', 'choice__speedrun__name')
+	raw_id_fields = ('choice',)
+
+class ChoiceInline(admin.StackedInline):
+	model = tracker.models.Choice;
+	extra = 0
+
+class ChoiceAdmin(admin.ModelAdmin):
+	search_fields = ('name', 'speedrun__name', 'description');
+	raw_id_fields = ('speedrun',)
+	list_filter = ('speedrun__event', 'state')
+	inlines = [ChoiceOptionInline]
+
+class DonationInline(admin.StackedInline):
+	model = tracker.models.Donation
+	extra = 0
 
 class DonationAdmin(admin.ModelAdmin):
 	list_display = ('donor', 'amount', 'timereceived', 'event', 'domain', 'transactionstate', 'bidstate', 'readstate', 'commentstate',)
 	search_fields = ('donor__email', 'donor__alias', 'donor__firstname', 'donor__lastname', 'amount')
+	list_filter = ('event', 'transactionstate', 'readstate', 'commentstate', 'bidstate')
+	raw_id_fields = ('donor',)
 
 class DonorAdmin(admin.ModelAdmin):
 	search_fields = ('email', 'alias', 'firstname', 'lastname')
+	inlines = [DonationInline]
 
 class EventAdmin(admin.ModelAdmin):
 	search_fields = ('short', 'name');
@@ -44,6 +70,7 @@ class PrizeAdmin(admin.ModelAdmin):
 	list_display = ('name', 'category', 'sortkey', 'bidrange', 'games', 'starttime', 'endtime', 'sumdonations', 'randomdraw', 'pin', 'event', 'winner' )
 	list_filter = ('event', 'category')
 	search_fields = ('name', 'description', 'winner__firstname', 'winner__lastname', 'winner__alais', 'winner__email')
+	raw_id_fields = ['startrun', 'endrun']
 	def bidrange(self, obj):
 		s = unicode(obj.minimumbid)
 		if obj.minimumbid != obj.maximumbid:
@@ -58,6 +85,8 @@ class PrizeAdmin(admin.ModelAdmin):
 
 class SpeedRunAdmin(admin.ModelAdmin):
 	search_fields = ['name', 'description']	
+	list_filter = ['event']
+	inlines = [ChoiceInline, ChallengeInline]
 
 admin.site.register(tracker.models.Challenge, ChallengeAdmin)
 admin.site.register(tracker.models.ChallengeBid, ChallengeBidAdmin)
