151be87290aa0a8b3942b1a453866d6e471417f3
smk <smk@sda.sorrowind.net>
2013-08-15 16:57:46 -0500

    - Added display for 'email sent' on the main prize pages - Changed the prize mail message for SGDQ (we really need to generalize that) - Added prizesheet.py, for putting the prizes into a csv file.  Not accessible from anywhere but the shell currently

---
+++ prizesheet.py
@@ -0,0 +1,26 @@
+import csv;
+import tracker.models as models;
+
+def GetAddress(donor):
+  addressParts = [];
+  if donor.addressstreet:
+    addressParts.append(donor.addressstreet);
+  if donor.addresscity:
+    addressParts.append(donor.addresscity);
+  if donor.addressstate:
+    addressParts.append(donor.addressstate);
+  if donor.addresscountry:
+    addressParts.append(donor.addresscountry);
+  if donor.addresszip:
+    addressParts.append(donor.addresszip);
+  return '\n'.join(addressParts);
+
+def WritePrizeSheet(event, filename):
+  prizes = models.Prize.objects.filter(event=event);
+  csvfile = open(filename, 'wb');
+  writer = csv.writer(csvfile, delimiter=',', quotechar='"');
+  writer.writerow(['Prize', 'Winner', 'Email', 'Address']);
+  for prize in prizes:
+    writer.writerow([prize.name.encode('utf-8'), (prize.winner.firstname + ' ' + prize.winner.lastname).encode('utf-8'), prize.winner.email.encode('utf-8'), GetAddress(prize.winner).encode('utf-8')]);
+  csvfile.close();
+
