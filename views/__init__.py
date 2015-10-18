04371aedf8f1588ca7088bcd2973c82bc6371261
Stephen Kiazyk <s_kiazyk@hotmail.com>
2015-10-18 16:53:40 -0500

    Transfer auth views to separate file

--- views/__init__.py
+++ views/__init__.py
@@ -67,75 +67,36 @@ import dateutil.parser
 import itertools
 import urllib2
 
-@never_cache
-def login(request):
-  message = None
-  if 'next' in request.GET:
-    message = 'Login required to continue.'
-  return auth_views.login(request, template_name='tracker/login.html', extra_context={'event': viewutil.get_event(None), 'csrftoken': get_csrf_token(request), 'message': message})
+from tracker.views.auth import __all__ as all_auth_views
+from tracker.views.auth import *
 
-@never_cache
-def logout(request):
-  auth_logout(request)
-  return django.shortcuts.redirect(request.META.get('HTTP_REFERER', settings.LOGOUT_REDIRECT_URL))
-
-@never_cache
-def password_reset(request):
-  return auth_views.password_reset(request,
-    template_name='tracker/password_reset.html',
-    email_template_name='password_reset_template',
-    password_reset_form=PostOfficePasswordResetForm,
-    from_email=settings.EMAIL_FROM_USER,
-    extra_context={'event': viewutil.get_event(None), 'csrftoken': get_csrf_token(request)})
-
-@never_cache
-def password_reset_done(request):
-  return tracker_response(request, 'tracker/password_reset_done.html')
-
-@never_cache
-def password_reset_confirm(request):
-  uidb64 = request.GET['uidb64']
-  token = request.GET['token']
-  return auth_views.password_reset_confirm(request,
-    uidb64,
-    token,
-    template_name='tracker/password_reset_confirm.html',
-    extra_context={'event': viewutil.get_event(None), 'csrftoken': get_csrf_token(request)})
-
-@never_cache
-def password_reset_complete(request):
-  return tracker_response(request, 'tracker/password_reset_complete.html', { 'login_url': reverse('login') })
-
-@never_cache
-@login_required
-def password_change(request):
-  return auth_views.password_change(request, template_name='tracker/password_change.html',extra_context={'csrftoken': get_csrf_token(request)})
-
-@never_cache
-@login_required
-def password_change_done(request):
-  return render(request, 'tracker/password_change_done.html')
-
-@never_cache
-def confirm_registration(request):
-  AuthUser = get_user_model()
-  uidb64 = request.GET.get('uidb64', None)
-  uid = urlsafe_base64_decode(uidb64) if uidb64 else None
-  token = request.GET.get('token',None)
-  user = None
-  tokenGenerator = default_token_generator
-  try:
-    user = AuthUser.objects.get(pk=uid)
-  except:
-    user = None
-  if request.method == 'POST':
-    form = RegistrationConfirmationForm(user=user, token=token, token_generator=tokenGenerator, data=request.POST)
-    if form.is_valid():
-      form.save()
-      return tracker_response(request, 'tracker/confirm_registration_done.html', {'user': form.user})
-  else:
-    form = RegistrationConfirmationForm(user=user, token=token, token_generator=tokenGenerator, initial={'userid': uid, 'authtoken': token, 'username': user.username if user else ''})
-  return tracker_response(request, 'tracker/confirm_registration.html', {'formuser': user, 'tokenmatches': tokenGenerator.check_token(user, token) if token else False, 'form': form, 'csrftoken': get_csrf_token(request)})
+__all__ = all_auth_views + [
+  'eventlist',
+  'index',
+  'search',
+  'add',
+  'edit',
+  'delete',
+  'bidindex',
+  'bid',
+  'donorindex',
+  'donor',
+  'donationindex',
+  'donation',
+  'runindex',
+  'run',
+  'prizeindex',
+  'prize',
+  'prize_donors',
+  'draw_prize',
+  'submit_prize',
+  'merge_schedule',
+  'refresh_schedule',
+  'paypal_cancel',
+  'paypal_return',
+  'donate',
+  'ipn',
+  ]
 
 def eventlist(request):
   return tracker_response(request, 'tracker/eventlist.html', { 'events' : Event.objects.all() })
@@ -160,17 +121,6 @@ def index(request,event=None):
     callback = request.GET['jsonp']
     return HttpResponse('%s(%s);' % (callback, json.dumps({'count':count,'agg':agg},ensure_ascii=False)), content_type='text/javascript;charset=utf-8')
   return tracker_response(request, 'tracker/index.html', { 'agg' : agg, 'count' : count, 'event': event })
-
-@never_cache
-def setusername(request):
-  if not request.user.is_authenticated or request.user.username[:10]!='openiduser' or request.method != 'POST':
-    return django.shortcuts.redirect(reverse('tracker.views.index'))
-  usernameform = UsernameForm(request.POST)
-  if usernameform.is_valid():
-    request.user.username = request.POST['username']
-    request.user.save()
-    return shortcuts.redirect(request.POST['next'])
-  return tracker_response(request, template='tracker/username.html', qdict={ 'usernameform' : usernameform })
 
 modelmap = {
   'bid'           : Bid,
