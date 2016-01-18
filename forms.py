964aa121729580e1106073d58bdd88c8b32b5221
SMK <s_kiazyk@hotmail.com>
2016-01-17 20:49:24 -0500

    Remove redundant check on custombid
    Its not very effectively enforcible

--- forms.py
+++ forms.py
@@ -141,9 +141,6 @@ class DonationBidForm(forms.Form):
           raise forms.ValidationError(_('Error, did not specify a name for the custom option.'))
         elif self.cleaned_data['amount'] < Decimal('1.00'):
           raise forms.ValidationError(_('Error, you must bid at least one dollar for a custom bid.'))
-      else:
-        if self.cleaned_data['customoptionname']:
-          raise forms.ValidationError(_('Error, specified custom data for a non-custom bid'))
     return self.cleaned_data
       
 class DonationBidFormSetBase(forms.BaseFormSet):
