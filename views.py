997eaa1d5b6f2b671fdf89254b459c596c02c52c
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2016-06-05 20:41:10 -0600

    migrate to better webpack

--- views.py
+++ views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from tracker.models import Event
 from django.views.decorators.csrf import csrf_protect
+from django.conf import settings
+from webpack_manifest import webpack_manifest
+
+import os
 
 @csrf_protect
 def index(request):
-	return render(request, 'tracker_ui/index.html', dictionary={'event': Event.objects.latest(), 'events': Event.objects.all()})
+    webpack = bool(request.META.get('HTTP_X_WEBPACK', False))
+    admin = webpack_manifest.load(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), 'ui-admin.manifest.json')),
+        '/assets' if webpack else (settings.STATIC_URL + '/gen'),
+        debug=settings.DEBUG,
+        timeout=60,
+        read_retry=None
+    )
+
+    return render(request, 'tracker_ui/index.html', dictionary={'event': Event.objects.latest(), 'events': Event.objects.all(), 'admin': admin})
