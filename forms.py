4598c52164cfdff1b255424d1c3f01a4a24ff289
Jason Deng <jdeng@hisa.local>
2017-06-30 01:43:58 -0500

    Add Charity opt-in

--- forms.py
+++ forms.py
@@ -106,6 +106,8 @@ class DonationEntryForm(forms.Form):
             max_length=32, label='Preferred Alias', required=False)
         self.fields['requestedemail'] = forms.EmailField(
             max_length=128, label='Preferred Email', required=False)
+        self.fields['requestedsolicitemail'] = forms.ChoiceField(
+            initial='CURR', choices=models.Donation._meta.get_field('requestedsolicitemail').choices, label='Charity Email Opt-In')
 
     def clean(self):
         if self.cleaned_data['requestedvisibility'] == 'ALIAS' and not self.cleaned_data['requestedalias']:
