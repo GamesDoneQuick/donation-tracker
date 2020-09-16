676333d7d125433e7c84c3d9b70a436fb4ac8c82
Benjamin Cutler <uraniumanchor@users.noreply.github.com>
2020-09-16 10:59:49 -0600

    add an endpoint for donation websocket (#255)
    [#174736894]

--- tracker/routing.py
+++ tracker/routing.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
+    path('ws/donations/', consumers.DonationConsumer),
     path('ws/ping/', consumers.PingConsumer),
 ]
