a973a9088d4ef820d422464cb93cd72352dfade4
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2020-11-05 12:02:24 -0700

    upgrades to make channels 3 work

--- tracker/routing.py
+++ tracker/routing.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/donations/', consumers.DonationConsumer),
-    path('ws/ping/', consumers.PingConsumer),
-    path('ws/celery/', consumers.CeleryConsumer),
+    path('ws/donations/', consumers.DonationConsumer.as_asgi()),
+    path('ws/ping/', consumers.PingConsumer.as_asgi()),
+    path('ws/celery/', consumers.CeleryConsumer.as_asgi()),
 ]
