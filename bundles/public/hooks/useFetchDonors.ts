bdc5db8585fa5f1827e3c812f11fcf27a39c6956
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2020-11-15 21:58:26 -0700

    wip - throttle model fetch

--- bundles/public/hooks/useFetchDonors.ts
+++ bundles/public/hooks/useFetchDonors.ts
@@ -1,26 +1,35 @@
 import modelActions from '../api/actions/models';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import * as React from 'react';
 
 export function useFetchDonors(eventId: number | string | undefined) {
-  const donations = useSelector((state: any) => state.models.donation);
-  const donors = useSelector((state: any) => state.models.donor);
+  const { donation: donations, donor: donors } = useSelector((state: any) => state.models);
+  const { donor: donorStatus } = useSelector((state: any) => state.status);
   const dispatch = useDispatch();
+  const [timeoutId, setTimeoutId] = React.useState<ReturnType<typeof setTimeout> | null>(null);
 
-  useEffect(() => {
-    if (!donors && eventId != null) {
-      dispatch(modelActions.loadModels('donor', { event: +eventId }));
-    } else if (donations) {
-      const ids = new Set(
-        donations
-          .filter(
-            (dn: any) => dn.donor && dn.donor__visibility !== 'ANON' && !donors?.find((dr: any) => dr.pk === dn.donor),
-          )
-          .map((dn: any) => dn.donor),
+  React.useEffect(() => {
+    if (!timeoutId && donorStatus !== 'loading') {
+      setTimeoutId(
+        setTimeout(() => {
+          if (!donors && eventId != null) {
+            dispatch(modelActions.loadModels('donor', { event: +eventId }));
+          } else if (donations) {
+            const ids = new Set(
+              donations
+                .filter(
+                  (dn: any) =>
+                    dn.donor && dn.donor__visibility !== 'ANON' && !donors?.find((dr: any) => dr.pk === dn.donor),
+                )
+                .map((dn: any) => dn.donor),
+            );
+            if (ids.size) {
+              dispatch(modelActions.loadModels('donor', { ids: [...ids.values()].join(',') }, true));
+            }
+          }
+          setTimeoutId(null);
+        }, 0),
       );
-      if (ids.size) {
-        dispatch(modelActions.loadModels('donor', { ids: [...ids.values()].join(',') }, true));
-      }
     }
-  }, [dispatch, donations, donors, eventId]);
+  }, [dispatch, donations, donors, donorStatus, eventId, timeoutId]);
 }
