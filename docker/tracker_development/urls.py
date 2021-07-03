cdb8ae04a90e75fb1eb1bcd19120b3255416c3fe
Keiichiro Amemiya <hoishinxii@gmail.com>
2021-07-04 07:55:42 +0900

    chore: add docker-compose to run tracker locally

---
+++ docker/tracker_development/urls.py
@@ -0,0 +1,26 @@
+"""tracker_development URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+import tracker.urls
+import ajax_select.urls
+
+urlpatterns = [
+    path('tracker/admin/', admin.site.urls),
+    path('tracker/admin/lookups/', include(ajax_select.urls)),
+    path('tracker/', include(tracker.urls, namespace='tracker')),
+]
