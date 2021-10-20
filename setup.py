dc5ed017ea280c22576a55d64c9111bdb11f783d
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2021-10-20 12:45:09 -0600

    django-paypal 1.0.0 version lock for now
    - django-paypal 1.1 changed the form in a way that breaks our flow
      because of use of an undocumented api

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
         'django-ajax-selects~=1.9',
         'django-ical~=1.7',
         'django-mptt~=0.10',
-        'django-paypal~=1.0',
+        'django-paypal~=1.0.0',
         'django-post-office~=3.2',
         'django-timezone-field>=3.1,<5.0',
         'djangorestframework~=3.9',
