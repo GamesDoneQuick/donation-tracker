778f6a4e6696bd41cefd4f50987eab30b80c9c85
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2023-01-08 16:15:56 -0500

    wip - total watch shows donation intervals

--- bundles/admin/totalWatch.tsx
+++ bundles/admin/totalWatch.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
-import moment from 'moment';
+import moment, { Moment } from 'moment';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
+
+import { useConstants } from '@common/Constants';
+import { paginatedFetch } from '@public/api/actions/paginate';
 
 import modelActions from '../public/api/actions/models';
 
@@ -32,6 +35,12 @@ type Bid = {
   state: string;
 };
 
+type Donation = {
+  pk: number;
+  amount: number;
+  timereceived: Moment;
+};
+
 type Speedrun = {
   pk: number;
   order: number | null;
@@ -44,11 +53,44 @@ function bidsReducer(state: Bid[], action: Bid[]) {
   return action.reduce((bids, bid) => bids.filter(b => b.pk !== bid.pk).concat([bid]), state);
 }
 
+const intervals = [5, 15, 30, 60, 180];
+
+type IntervalData = {
+  run: number;
+  intervals: { [k: number]: number };
+};
+
 export default React.memo(function TotalWatch() {
   const { event: eventId } = useParams<{ event: string }>();
   const event = useSelector((state: any) => state.models.event?.find((e: any) => e.pk === +eventId!));
   const runs = useSelector((state: any) => state.models.speedrun) as Speedrun[];
+  // TODO: use the model state
+  const { API_ROOT } = useConstants();
+  const [apiDonations, setApiDonations] = React.useState<Donation[]>([]);
+  const [feedDonations, setFeedDonations] = React.useState<Donation[]>([]);
   const currentRun = React.useMemo(() => runs?.find(r => moment().isBetween(r.starttime, r.endtime)), [runs]);
+  const donations = React.useMemo(
+    () => [...feedDonations, ...apiDonations.filter(ad => !feedDonations.some(fd => fd.pk === ad.pk))],
+    [apiDonations, feedDonations],
+  );
+  const intervalData = React.useMemo(() => {
+    const now = moment();
+    return donations.reduce(
+      (intervalData, donation) => {
+        const timereceived = moment(donation.timereceived);
+        if (currentRun && timereceived.isSameOrAfter(currentRun.starttime)) {
+          intervalData.run += +donation.amount;
+        }
+        intervals.forEach(i => {
+          if (timereceived.isSameOrAfter(now.clone().subtract(i, 'minutes'))) {
+            intervalData.intervals[i] = (intervalData.intervals[i] || 0) + +donation.amount;
+          }
+        });
+        return intervalData;
+      },
+      { run: 0, intervals: {} } as IntervalData,
+    );
+  }, [currentRun, donations]);
 
   const [total, setTotal] = React.useState(0);
   React.useEffect(() => {
@@ -93,6 +135,15 @@ export default React.memo(function TotalWatch() {
   }, [bids, runs]);
   const dispatch = useDispatch();
   React.useEffect(() => {
+    const ago = moment().subtract(3, 'hours');
+    const timestamp = currentRun ? moment.min(moment(currentRun.starttime), ago) : ago;
+    paginatedFetch(`${API_ROOT}search/?type=donation&time_gte=${timestamp.toISOString()}`).then(data => {
+      setApiDonations(
+        data.map((d: any) => ({ pk: d.pk, amount: d.fields.amount, timereceived: moment(d.fields.timereceived) })),
+      );
+    });
+  }, [API_ROOT, currentRun, dispatch, eventId]);
+  React.useEffect(() => {
     dispatch(modelActions.loadModels('speedrun', { event: eventId }));
     const interval = setInterval(() => {
       dispatch(modelActions.loadModels('speedrun', { event: eventId }));
@@ -129,6 +180,17 @@ export default React.memo(function TotalWatch() {
       data = JSON.parse(data);
       setTotal(data.new_total as number);
       dispatchBids(data.bids as Bid[]);
+      setFeedDonations(donations =>
+        donations
+          .filter(d => d.pk !== data.id)
+          .concat([
+            {
+              pk: data.id,
+              amount: data.amount,
+              timereceived: moment(data.timereceived),
+            },
+          ]),
+      );
     });
     setSocket(socket);
   }, [dispatch, eventId]);
@@ -144,7 +206,17 @@ export default React.memo(function TotalWatch() {
         Socket State: {socketState(socket)} {retry.current > 0 && `(${retry.current})`}
       </div>
       {total && <h2>Total: ${format.format(total)}</h2>}
-      {currentRun && <h3>Current Run: {currentRun.name}</h3>}
+      {currentRun && (
+        <>
+          <h3>Current Run: {currentRun.name}</h3>
+          <h4>Total since run start: ${format.format(intervalData.run)}</h4>
+        </>
+      )}
+      {Object.entries(intervalData.intervals).map(([k, v]) => (
+        <h4 key={k}>
+          Total in the last {k} minutes: ${format.format(v)}
+        </h4>
+      ))}
       {sortedBids?.map(bid => {
         const speedrun = runs?.find(r => bid.speedrun === r.pk);
         return bid.parent ? (
