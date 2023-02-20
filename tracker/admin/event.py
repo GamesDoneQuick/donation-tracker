6bec5346f6ce824da251befaa149bd8486d2b8b0
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2023-02-20 16:10:50 -0700

    add Milestones to public pages
    [#184497110]

--- tracker/admin/event.py
+++ tracker/admin/event.py
@@ -216,6 +216,7 @@ class EventAdmin(CustomModelAdmin):
                     # needed for 'Start Run'
                     'change_speedrun',
                     'view_speedrun',
+                    # milestones
                     'view_milestone',
                 ]
                 admin_permissions = auth.Permission.objects.filter(
