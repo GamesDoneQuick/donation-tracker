b51519884fe0fe9e325d978e8a3efaddc6978664
Jon <jonegeland@gmail.com>
2023-04-16 21:28:12 -0700

    Add `me` endpoint for loading current user data (#529)
    add tests

---
+++ bundles/processing/AuthStore.tsx
@@ -0,0 +1,25 @@
+import create from 'zustand';
+
+import { Me } from '@public/apiv2/APITypes';
+
+interface AuthStoreState {
+  me: Me | null;
+}
+
+const useAuthStore = create<AuthStoreState>()(() => ({
+  me: null,
+}));
+
+export default useAuthStore;
+
+export function useMe() {
+  return useAuthStore(state => state.me);
+}
+
+export function loadMe(me: Me) {
+  useAuthStore.setState({ me });
+}
+
+export function logout() {
+  useAuthStore.setState({ me: null });
+}
