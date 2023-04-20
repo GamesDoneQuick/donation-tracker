a4436d918bb093de769dc7c13cdc0257c7c12793
Jon <jonegeland@gmail.com>
2023-04-19 21:30:15 -0700

    Support reordering of donations within groups on Read Donations (#543)
    * fix adding/removing with donation groups
    
    * Enable drag and drop for donations within groups

---
+++ bundles/uikit/icons/DragHandle.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { faGripVertical } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const DragHandle = (props: any) => {
+  return <FontAwesomeIcon {...props} icon={faGripVertical} />;
+};
+
+export default DragHandle;
