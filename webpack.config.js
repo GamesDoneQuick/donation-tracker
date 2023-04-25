8e2b6c631a7f20affd4de1a1b58f2be55a46953a
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2023-04-25 14:49:07 -0600

    wip - misc fixes

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ const SOURCE_MAPS = process.env.SOURCE_MAPS ?? false;
 const ANALYZE = process.env.ANALYZE ?? false;
 const NO_MANIFEST = !!process.env.NO_MANIFEST ?? false;
 const PROJECT_ROOT = __dirname;
+const STATIC_ROOT = process.env.STATIC_ROOT ?? '/static/gen';
 
 console.log(PROD ? 'PRODUCTION BUILD' : 'DEVELOPMENT BUILD');
 
@@ -47,7 +48,7 @@ module.exports = {
     filename: PROD ? 'tracker-[name]-[contenthash].js' : 'tracker-[name].js',
     pathinfo: true,
     path: PROJECT_ROOT + '/tracker/static/gen',
-    publicPath: '/static/gen',
+    publicPath: STATIC_ROOT,
   },
   stats: 'minimal',
   module: {
