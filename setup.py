0e03f1bb06f9b5621a1fa6c34b7358ce279a57b8
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2023-04-25 14:49:07 -0600

    wip - misc fixes

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ os.chdir(old_dir)
 
 setup(
     name='django-donation-tracker',
-    version='3.1',
+    version='3.1.0',
     author='Games Done Quick',
     author_email='tracker@gamesdonequick.com',
     packages=find_packages(include=['tracker', 'tracker.*']),
