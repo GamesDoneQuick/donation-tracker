8a4fb164632bd531173a1f1a252026fa1f2ecb1c
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2023-05-10 17:12:31 -0600

    donation postbacks include bid information
    [#185152880]

--- tracker/eventutil.py
+++ tracker/eventutil.py
@@ -22,6 +22,7 @@ def post_donation_to_postbacks(donation):
 
     data = {
         'id': donation.id,
+        'event': donation.event_id,
         'timereceived': str(donation.timereceived),
         'comment': donation.comment,
         'amount': float(donation.amount),
@@ -29,6 +30,18 @@ def post_donation_to_postbacks(donation):
         'donor__visiblename': donation.donor.visible_name(),
         'new_total': float(total),
         'domain': donation.domain,
+        'bids': [
+            {
+                'id': db.bid.id,
+                'total': float(db.bid.total),
+                'parent': db.bid.parent_id,
+                'name': db.bid.name,
+                'goal': float(db.bid.goal) if db.bid.goal else None,
+                'state': db.bid.state,
+                'speedrun': db.bid.speedrun_id,
+            }
+            for db in donation.bids.select_related('bid')
+        ],
     }
 
     async_to_sync(get_channel_layer().group_send)(
