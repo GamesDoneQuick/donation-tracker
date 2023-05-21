7f50d69f809df5c48dd3d4f8f3fd43118886b1b3
Jon <jonegeland@gmail.com>
2023-05-20 20:08:49 -0700

    allow ProcessingSocket url to be set from config (#571)

---
+++ bundles/public/apiv2/sockets/index.tsx
@@ -0,0 +1,28 @@
+import { ProcessingSocketImpl } from './ProcessingSocket';
+
+let socketRoot: string | undefined;
+let processingSocket: ProcessingSocketImpl | undefined;
+
+function getAbsoluteSocketURL(path: string) {
+  if (socketRoot == null) {
+    throw 'setSocketRoot must be called with a value for the common websocket path before requesting a websocket.';
+  }
+
+  // If the root doesn't start with a slash, assume it's a fully-formed URL already.
+  if (!socketRoot.startsWith('/')) return socketRoot;
+
+  const protocol = window.location.protocol === 'https' ? 'wss' : 'ws';
+
+  return `${protocol}://${window.location.host}${socketRoot}${path}`;
+}
+
+export const sockets = new (class {
+  setSocketRoot(path: string) {
+    socketRoot = path;
+  }
+
+  get processingSocket(): ProcessingSocketImpl {
+    processingSocket ??= new ProcessingSocketImpl(getAbsoluteSocketURL('processing/'));
+    return processingSocket;
+  }
+})();
