5e47e7b4063e9c7f9018b8e532c4f82024a4ae1e
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2023-05-28 12:42:15 -0500

    wip - permissions fixes for admin

--- tracker/admin/util.py
+++ tracker/admin/util.py
@@ -140,7 +140,8 @@ class DonationStatusMixin:
         if not request.user.has_perm('tracker.view_pending_donation'):
             list_display.remove('transactionstate')
         if not request.user.has_perm('tracker.view_test'):
-            list_display.remove('testdonation')
+            if 'testdonation' in list_display:
+                list_display.remove('testdonation')
         return list_display
 
     def get_list_filter(self, request):
@@ -151,5 +152,6 @@ class DonationStatusMixin:
             not request.user.has_perm('tracker.view_pending_donation')
             and 'donation__testdonation' in list_filter
         ):
-            list_filter.remove('donation__testdonation')
+            if 'donation__testdonation' in list_filter:
+                list_filter.remove('donation__testdonation')
         return list_filter
