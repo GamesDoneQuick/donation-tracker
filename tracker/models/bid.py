70219e84fdaaa1f3f338d166570c76b50755f50c
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2023-05-29 14:21:14 -0500

    wip - child state propagation

--- tracker/models/bid.py
+++ tracker/models/bid.py
@@ -536,6 +536,7 @@ class Bid(mptt.models.MPTTModel):
             self.parent.save()
 
     def check_parent(self):
+        self.parent.refresh_from_db()
         changed = False
         if self.speedrun != self.parent.speedrun:
             self.speedrun = self.parent.speedrun
