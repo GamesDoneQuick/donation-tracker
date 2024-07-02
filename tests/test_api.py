3eca97dbf3ca519fb8d65bf2b7718e8ea5c25511
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2024-07-02 15:41:47 -0500

    wip - ordered runs must have length

--- tests/test_api.py
+++ tests/test_api.py
@@ -150,6 +150,7 @@ class TestSpeedRun(APITestCase):
                 name=run.name,
                 onsite=run.onsite,
                 order=run.order,
+                priority_tag=run.priority_tag_id,
                 public=str(run),
                 release_year=run.release_year,
                 run_time=run.run_time,
@@ -158,6 +159,7 @@ class TestSpeedRun(APITestCase):
                 starttime=(
                     format_time(run.starttime) if run.starttime else run.starttime
                 ),
+                tags=[t.id for t in run.tags.all()],
                 twitch_name=run.twitch_name,
                 priority_tag=run.priority_tag_id,
                 tags=(t.id for t in run.tags.all()),
