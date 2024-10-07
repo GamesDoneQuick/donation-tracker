d680eebf66351a4fe824f61396236ec0438d44dd
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2024-10-07 14:22:04 -0600

    permissions tweaks to Bids
    - view_hidden_bid is a last resort, view_bid or change_bid are now sufficient
    
    [#188391351]

--- tests/apiv2/test_bids.py
+++ tests/apiv2/test_bids.py
@@ -297,6 +297,7 @@ class TestBidViewSet(TestBidBase, APITestCase):
                     },
                     status_code=400,
                     expected_error_codes={'repeat': 'invalid'},
+                    expected_error_codes={'repeat': 'invalid'},
                 )
 
             with self.subTest('require locked permission'):
