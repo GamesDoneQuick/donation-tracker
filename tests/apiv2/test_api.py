6d503bf31e940328b12ccc14aa16ac569093f21c
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2025-01-09 15:49:40 -0500

    wip - validate endpoint

--- tests/apiv2/test_api.py
+++ tests/apiv2/test_api.py
@@ -1,4 +1,7 @@
+from django.urls import reverse
+
 from tests.util import APITestCase
+from tracker import models
 from tracker.api import messages
 
 
@@ -37,3 +40,79 @@ class TestAPI(APITestCase):
             status_code=400,
             expected_error_codes={'runners': messages.NO_NESTED_CREATES_CODE},
         )
+
+    def test_validate(self):
+        with self.subTest('creates'), self.assertLogsChanges(0):
+            self.post_noun(
+                'validate',
+                model_name='interview',
+                status_code=202,
+                view_name='validate-create',
+                data={
+                    'event': self.event.pk,
+                    'order': 1,
+                    'suborder': 1,
+                    'topic': 'Foo',
+                    'interviewers': 'Bar',
+                    'length': '5:00',
+                },
+                user=self.super_user,
+            )
+            data = self.post_noun(
+                'validate',
+                model_name='interview',
+                status_code=400,
+                view_name='validate-create',
+                data=[
+                    {
+                        'event': self.event.pk,
+                        'order': 1,
+                        'suborder': 1,
+                        'topic': 'Foo',
+                        'interviewers': 'Bar',
+                        'length': '5:00',
+                    },
+                    {},
+                ],
+            )
+            self.assertIsNone(data['valid'][1])
+            self.assertIsNone(data['invalid'][0])
+            self.assertIsInstance(data['valid'][0], dict)
+            self.assertIsInstance(data['invalid'][1], dict)
+
+        with self.subTest('updates'), self.assertLogsChanges(0):
+            interview = models.Interview.objects.create(
+                event=self.event, order=1, suborder=1, interviewers='Foo', topic='Bar'
+            )
+            self.patch_noun(
+                interview,
+                'validate',
+                model_name='interview',
+                view_name='validate-update',
+                data={'topic': 'Changed Topic'},
+                status_code=202,
+            )
+            self.patch_noun(
+                interview,
+                'validate',
+                model_name='interview',
+                view_name='validate-update',
+                data={},
+                status_code=202,
+            )
+
+            self.patch_noun(
+                interview,
+                'validate',
+                model_name='interview',
+                view_name='validate-update',
+                data={'topic': ''},
+                status_code=400,
+            )
+
+            resp = self.client.patch(
+                reverse('tracker:api_v2:interview-validate-update', args=(123456,)),
+                data={},
+                format='json',
+            )
+            self.assertEqual(resp.status_code, 404)
--- tests/util.py
+++ tests/util.py
@@ -376,7 +376,9 @@ class APITestCase(TransactionTestCase, AssertionHelpers, AssertionModelHelpers):
         paginator.paginate_queryset(queryset, FakeRequest())
         return paginator.get_paginated_response(data)
 
-    def _get_viewname(self, model_name, action, **kwargs):
+    def _get_viewname(self, model_name, action, view_name=None, **kwargs):
+        if view_name:
+            return f'tracker:api_v2:{model_name}-{view_name}'
         if 'event_pk' in kwargs:
             if 'feed' in kwargs:
                 viewname = f'tracker:api_v2:event-{model_name}-feed-{action}'
@@ -440,7 +442,7 @@ class APITestCase(TransactionTestCase, AssertionHelpers, AssertionModelHelpers):
         model_name = model_name or self.model_name
         assert model_name is not None
         url = reverse(
-            self._get_viewname(model_name, 'list', **kwargs),
+            self._get_viewname(model_name, 'list'),
             kwargs=kwargs,
         )
         with self.process_snapshot('GET', url, data) as snapshot:
@@ -477,7 +479,7 @@ class APITestCase(TransactionTestCase, AssertionHelpers, AssertionModelHelpers):
             self.client.force_authenticate(user=user)
         model_name = model_name or self.model_name
         assert model_name is not None
-        url = reverse(self._get_viewname(model_name, noun, **kwargs), kwargs=kwargs)
+        url = reverse(self._get_viewname(model_name, noun), kwargs=kwargs)
         with self.process_snapshot('GET', url, data) as snapshot:
             response = self.client.get(
                 url,
@@ -594,6 +596,7 @@ class APITestCase(TransactionTestCase, AssertionHelpers, AssertionModelHelpers):
         kwargs=None,
         expected_error_codes=None,
         user=_empty,
+        view_name=None,
     ):
         kwargs = kwargs or {}
         data = data or {}
@@ -601,7 +604,7 @@ class APITestCase(TransactionTestCase, AssertionHelpers, AssertionModelHelpers):
             self.client.force_authenticate(user=user)
         model_name = model_name or self.model_name
         assert model_name is not None
-        url = reverse(self._get_viewname(model_name, noun, **kwargs), kwargs=kwargs)
+        url = reverse(self._get_viewname(model_name, noun, view_name), kwargs=kwargs)
         with self.process_snapshot('POST', url, data) as snapshot:
             response = self.client.post(
                 url,
@@ -627,6 +630,7 @@ class APITestCase(TransactionTestCase, AssertionHelpers, AssertionModelHelpers):
     ):
         return self.patch_noun(
             obj,
+            'detail',
             model_name=model_name,
             status_code=status_code,
             expected_error_codes=expected_error_codes,
@@ -638,9 +642,9 @@ class APITestCase(TransactionTestCase, AssertionHelpers, AssertionModelHelpers):
     def patch_noun(
         self,
         obj,
+        noun,
         *,
         model_name=None,
-        noun='detail',
         status_code=200,
         expected_error_codes=None,
         data=None,
