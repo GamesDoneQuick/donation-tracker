2fd4aa25835ef50923602bde5f7b9955f99963ff
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2025-01-12 16:58:03 -0500

    wip - v1 is dead

--- tracker/api_urls.py
+++ tracker/api_urls.py
@@ -4,12 +4,12 @@ from tracker.views import api
 
 app_name = 'tracker'
 urlpatterns = [
-    path('', api.root, name='root'),
-    path('search/', api.search, name='search'),
+    path('', api.gone, name='root'),
+    path('search/', api.gone, name='search'),
     path('add/', api.gone, name='add'),
     path('edit/', api.gone, name='edit'),
     path('delete/', api.gone, name='delete'),
-    path('command/', api.command, name='command'),
+    path('command/', api.gone, name='command'),
     path('me/', api.gone, name='me'),
     path('ads/<int:event>/', api.gone, name='ads'),
     path('interviews/<int:event>/', api.gone, name='interviews'),
