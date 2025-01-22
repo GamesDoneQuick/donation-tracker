4b1b43040e71de4f06fc1b6179a53ee6f41a6a5b
Benjamin Cutler <benjamincutler@gamesdonequick.com>
2025-01-21 19:01:48 -0700

    wip - sites in selenium tests

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -7,6 +7,7 @@ from unittest import skipIf
 from django.contrib.admin.options import IncorrectLookupParameters
 from django.contrib.auth import get_user_model
 from django.contrib.auth import models as auth_models
+from django.contrib.sites.models import Site
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 from selenium.common import StaleElementReferenceException
@@ -76,6 +77,7 @@ def retry(n_or_func):
 class ProcessDonationsAndBidsBrowserTest(TrackerSeleniumTestCase):
     def setUp(self):
         User = get_user_model()
+        Site.objects.create(domain='localhost', name='Local Server')
         self.rand = random.Random(None)
         self.superuser = User.objects.create_superuser(
             'superuser',
