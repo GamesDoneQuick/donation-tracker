0c6ff1a6b69cf2426e4b93def79a396e85bf94f2
Benjamin Cutler <uraniumanchor@users.noreply.github.com>
2025-06-19 17:59:28 -0600

    packaging updates (#858)
    - build packages with JS production dependencies only, to verify correctness
    - move more package information into pyproject.toml
    - tag artifacts with build number instead of hash
    - get package version from package itself and verify Python and NPM versions
      are in sync

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -1,5 +1,25 @@
-from .settings import TrackerSettings
+import json
+import os
 
-settings = TrackerSettings()
+try:
+    from .settings import TrackerSettings
 
-default_app_config = 'tracker.apps.TrackerAppConfig'
+    settings = TrackerSettings()
+
+    default_app_config = 'tracker.apps.TrackerAppConfig'
+except ImportError:
+    # happens when setuptools is trying to evaluate the version
+    pass
+
+__bare_version__ = '3.3.1.dev0'
+
+if __tag__ := os.environ.get('BUILD_NUMBER', ''):
+    __version__ = __bare_version__.replace('dev0', __tag__)
+else:
+    __version__ = __bare_version__
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), '../package.json')) as pkg:
+        assert json.load(pkg)['version'] == __bare_version__
+except IOError:
+    pass
